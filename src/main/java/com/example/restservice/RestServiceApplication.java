d114fd3df32938d9e70e6bea8ec93d99038746b7
Ilgam Shakirov <shilgam@gmail.com>
2020-09-06 00:26:22 +0300

    Update service and artifact name

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.restapi;
+package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class RestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiApplication.class, args);
+		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/restservice/RestServiceApplicationTests.java
+++ src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.restapi;
+package com.example.restservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestApiApplicationTests {
+class RestServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
