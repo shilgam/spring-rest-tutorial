1c528d054fde44a810737f85dad8e4fa86aad917
Ilgam Shakirov <shilgam@gmail.com>
2020-09-07 19:45:16 +0300

    Change app name: 'payroll'

--- src/main/java/com/example/payroll/PayrollApplication.java
+++ src/main/java/com/example/payroll/PayrollApplication.java
@@ -1,13 +1,13 @@
-package com.example.restservice;
+package com.example.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class PayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+		SpringApplication.run(PayrollApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/payroll/PayrollApplicationTests.java
+++ src/test/java/com/example/payroll/PayrollApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.restservice;
+package com.example.payroll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestServiceApplicationTests {
+class PayrollApplicationTests {
 
 	@Test
 	void contextLoads() {
